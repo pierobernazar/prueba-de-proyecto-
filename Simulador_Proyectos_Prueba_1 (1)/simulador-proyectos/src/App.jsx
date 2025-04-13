678181e6671097654552e0abd95a5a9a8e8a0fb9
pierobernazar <bernazar.piero@gmail.com>
2025-04-13 13:57:06 -0400

    Add files via upload

---
+++ Simulador_Proyectos_Prueba_1 (1)/simulador-proyectos/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Simulador de Evaluación de Proyectos</h1>
+      <p>Este es un ejemplo funcional básico. Personaliza con tus preguntas.</p>
+    </div>
+  );
+}
+
+export default App;
